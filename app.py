5459afe34fcc0a460fd3d1b0e5058140f1862bc6
Shikhar Mishra <shikharmishra1031@gmail.com>
2025-05-21 13:24:54 +0530

    ffjnj

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ login_manager.login_view = 'auth.login'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_app_password'  # Use App Password for Gmail
+app.config['MAIL_USERNAME'] = 'scrapeease@gmail.com'
+app.config['MAIL_PASSWORD'] = 'epoa qajq dyeu dadi'  # Use App Password for Gmail
 mail = Mail(app)
 
 
