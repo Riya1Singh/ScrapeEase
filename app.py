eaab4913a96da91c490375da3a425cea0dead697
Shikhar Mishra <shikharmishra1031@gmail.com>
2025-05-21 11:46:27 +0530

    hggh

--- app.py
+++ app.py
@@ -517,4 +517,4 @@ def protected():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=False) 
+    app.run(debug=False,host='0.0.0.0') 
